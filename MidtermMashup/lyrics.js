9987a540d5809dd0172a7b2941c7d229d1834600
Tseki Lhamo <tseki@Tsekis-MacBook-Air.local>
2019-10-27 20:05:29 -0500

    MidtermMashup

---
+++ MidtermMashup/lyrics.js
@@ -0,0 +1,44 @@
+﻿
+var musixtoken = "f675e6b3016cca4313771ec978c56751";
+var musixbaseurl = "https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/";
+//var fullURL = "http://api.musixmatch.com/ws/1.1/track.search?q_artist=kanye&q_track=ultralight&apikey=" + token;
+//var fullURL = url + "artist.search?s_artist_rating=desc&q_artist=coldplay&page=1&page_size=5";
+let musixh = new Headers();
+//musixh.append('Authorization', musixtoken);
+
+async function showLyrics(button) {
+    clearLyrics();
+    var buttonText = button.innerText;
+    var fullText = button.parentElement.innerText;
+    var trackname = fullText.replace(buttonText, '');
+    // get the track to get it's track id
+    var searchURL = musixbaseurl + 'track.search?q_track=' + trackname + '&apikey='+ musixtoken;
+    const req = await makeRequest(searchURL, musixh);
+    submit(setTrackID, req);
+}
+
+async function setTrackID(data) {
+    if (data.message.body.track_list.length == 0) {
+        console.log("Musixmatch does not have a trackID for this song. Please choose another song!");
+        alert("Musixmatch does not have a trackID for this song. Please choose another song!");
+    } else {
+        const trackid = data.message.body.track_list[0].track.track_id;
+        console.log(trackid);
+        var trackURL = musixbaseurl + 'track.lyrics.get?track_id=' + trackid + '&apikey=' + musixtoken;
+        const req = await makeRequest(trackURL, musixh);
+        submit(displayLyrics, req);
+    }
+}
+
+function displayLyrics(data) {
+    console.log(data);
+    if (data.message.body.length == 0) {
+        document.getElementById("lyrics").innerHTML = "OOps MusixMatch API does not have lyrics for this song! ☹️";
+    } else {
+        document.getElementById("lyrics").innerHTML = data.message.body.lyrics.lyrics_body;
+    }
+}
+
+function temp() {
+    document.getElementById("lyrics").innerHTML = "Yes! It's so crazy right now! Most incredibly, it's ya girl, Bee It's ya boy, young. You ready? Uh oh, uh oh, uh oh, oh no no Uh oh, uh oh, uh oh, oh no no Uh oh, uh oh, uh oh, oh no no Uh oh, uh oh, uh oh, oh no no Yea, history in the making, it's so crazy right now I look and stare so deep in your eyes I touch on you more and more every time When you leave I'm begging you not to go Call your name two or three times in a row Such a funny thing for me to try to explain How I'm feeling and my pride is the one to blame. 'Cuz I know I don't understand Just how your love your doing no one else can.Got me looking so crazy right now, your love's Got me looking so crazy right now (in love) Got me looking so crazy right now, your touch Got me looking so crazy right now (your touch) Got me hoping you'll page me right now, your kiss Got me hoping you'll save me right now Looking so crazy in love's Got me looking, got me looking so crazy in love.Uh oh, uh oh, uh oh, oh no no Uh oh, uh oh, uh oh, oh no no ... ******* This Lyrics is NOT for Commercial use ******* (1409618693831)";
+}
---
+++ MidtermMashup/spotify.js
@@ -0,0 +1,111 @@
+// Spotify API requirements 
+var spotifytoken = 'BQB2_CxSoH5whLcQfHGuyjZpd7HTUNDPh-XaQnHUJA0qdKvfPSKr-V3Iq9VG_1n2PPczqmxTDdvzaZ9ya0NZfhVZyh_6Sv7Ra2Jx3MwqXzT9WfddiQK4mWa6KGQ2GbUSPqEgqBMbPPZS7mSoGQ';
+//const uri = 'https://api.spotify.com/v1/playlists/37i9dQZF1DX9tPFwDMOaN1/tracks?fields=items(track(id%2Cname))';
+let spotifyh = new Headers();
+spotifyh.append('Authorization', 'Bearer ' + spotifytoken);
+spotifyh.append('Accept', 'application/json');
+spotifyh.append('Content-Type', 'application/json');
+
+// Base URL
+const spotifybaseurl = 'https://api.spotify.com/v1';
+
+const search_ex = "/search?";
+
+var art_ex = "/artists/";
+// This will change once search works
+var artistID;
+var artistTopTracksURL;
+var artistname;
+
+function makeRequest(url, h) {
+    req = new Request(url, {
+        method: 'GET',
+        headers: h,
+        mode: 'cors'
+    });
+    return req;
+}
+
+async function searchArtist() {
+    artistname = document.getElementById("artistname").value;
+    clearLyrics();
+    if (artistname == "") {
+        alert("Please enter an artistname");
+        clearFields();
+    } else {
+        console.log(artistname);
+        var searchURL = spotifybaseurl + search_ex + 'q=' + artistname + '&type=artist';
+        const req = await makeRequest(searchURL, spotifyh);
+        // Gets response and sets artist ID
+        submit(setArtistID, req);
+    }
+
+}
+
+// Sets the artistID and the toptracks url
+async function setArtistID(data) {
+    // Check if artist is valid
+    if (data.artists.items.length == 0) {
+        await alert("Could not find artist! Please try again!");
+        clearFields();
+    } else {
+        artistID = data.artists.items[0].id;
+        artistTopTracksURL = spotifybaseurl + art_ex + artistID + '/' + 'top-tracks?country=ES';
+        const req2 = await makeRequest(artistTopTracksURL, spotifyh)
+        submit(printTopSongs, req2);
+    }
+
+}
+                   // <li>${data.tracks[i].name}   <button id="showlyricsbtn" onclick="showLyrics(this)">Show Lyrics</button></li>
+
+// Prints the top tracks as a list
+function printTopSongs(data) {
+    if (data.tracks.length == 0) {
+        console.log(artistname + "does not have any top songs!");
+        alert(artistname + "does not have any top songs!");
+        clearFields();
+    } else {
+        // Capitalize first letter of artistname 
+        artistname = artistname[0].toUpperCase() +
+            artistname.slice(1);
+        let output = `<h2>Top Tracks for ${artistname}</h2>
+                   <ol>
+                 `;
+        for (var i = 0, len = data.tracks.length; i < len; i++) {
+            output += `
+                    <li>${data.tracks[i].name}   <button id="showlyricsbtn" onclick="temp()">Show Lyrics</button>
+                    </li>
+                       `
+        }
+        output += "</ol";
+        document.getElementById("song_list").innerHTML = output;
+    }
+}
+
+// Call Spotify API when submit button is clicked
+async function submit(callback, request) {
+        try {
+            const response = await fetch(request);
+            if (response.status != 200) {
+                console.log("Fetch was not successful! Spotify's OAuth Token only works for 1 hour. Please contact lhamo012@umn.edu to get a new access token for grading Mid-term Mashup!");
+            } else {
+                const data = await response.json();
+                callback(data);
+                console.log(data);
+            }
+        }
+        catch (err) {
+            console.log('fetch failed', err);
+        }
+}
+
+// Clear artist name
+function clearFields() {
+    document.getElementById('artistname').value = "";
+}
+
+// Clear lyrics 
+function clearLyrics() {
+    document.getElementById('lyrics').innerHTML = "";
+}
+
